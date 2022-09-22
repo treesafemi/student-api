b6ab3325a622dd89601183152e9785ad3a74198d
treesafemi <treesafemi@gmail.com>
2022-09-22 15:40:59 +0530

    changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,117 +35,123 @@ exports.register = async function (req, res) {
 
 // student sign_in
 exports.sign_in = function (req, res) {
+  // let returndata = {}
   console.log("req", req.body)
   User.findOne({
     email: req.body.email,
     hash_password: req.body.hash_password
-  }, function (err, user) {
-    console.log("user", err)
+  }, function (err, useres) {
+    console.log(useres, err)
     if (err) {
       return res.status(401).json({ message: err });
     }
-    if (!user) {
+    if (!useres) {
       return res.status(401).json({ message: 'Authentication failed. Invalid user or password.' });
     } else {
-      returndata = {}
-      var userPromise = Promise.all(user.addcourse.map(function (row) {
-        return CoursesModel.findOne({ "_id": mongoose.Types.ObjectId(row), "ActiveStatus": true })
+      console.log(useres,"****")
+      var UserPromise = Promise.all(useres.addCourses.map(function (row) {
+        return Courses.findOne({ "_id": mongoose.Types.ObjectId(row), "ActiveStatus": true })
       }))
-      userPromise.then(function (result) {
+      console.log(UserPromise, "poo")
+      UserPromise.then(function (result) {
         var coursename = {};
         coursename.Courses = result;
-        //.coursenameActiveStatus = user.ActiveStatus
-        returndata = {
-          addcourse: coursename,
-          userdata: user
-   } }) } 
-        return res.json({ user, token: jwt.sign(returndata, 'RESTFULAPIs') });
+        let returndata = {
+          addCourses: coursename,
+          userdata: useres
+        }
+        useres.status = (err,null)
+        console.log(returndata)
+        return res.json({ returndata, token: jwt.sign({ email: useres.email, fullName: useres.fullName, _id: useres._id }, 'RESTFULAPIs') });
+
       })
-    };
-  
-    
+    }
 
-    
+  });
+};
 
-    exports.loginRequired = function (req, res, next) {
-      if (req.user) {
-        next();
-      } else {
+exports.loginRequired = function (req, res, next) {
+  if (req.user) {
+    next();
+  } else {
 
-        return res.status(401).json({ message: 'Unauthorized user!!' });
-      }
-    };
-    exports.profile = function (req, res, next) {
-      if (req.user) {
-        res.send(req.user);
-        next();
-      }
-      else {
-        return res.status(401).json({ message: 'Invalid token' });
-      }
-    };
+    return res.status(401).json({ message: 'Unauthorized user!!' });
+  }
+};
+exports.profile = function (req, res, next) {
+  if (req.user) {
+    res.send(req.user);
+    next();
+  }
+  else {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
 
-    // get student profile using getById
-    exports.getById = function (req, res, next) {
-      if (req.params.id) {
-        User.findById(req.params.id, function (err, user) {
-          res.send(user);
-        })
-      }
-      else {
-        return res.status(401).json({ message: 'Invalid id' });
-      }
-    };
+// get student profile using getById
+exports.getById = function (req, res, next) {
+  if (req.params.id) {
+    User.findById(req.params.id, function (err, user) {
+      res.send(user);
+    })
+  }
+  else {
+    return res.status(401).json({ message: 'Invalid id' });
+  }
+};
 
-    // student profile update
-    exports.update = async (req, res, next) => {
-      const id = req.params.ids;
-      if (id) {
-        User.findByIdAndUpdate(id, req.body, (err, result) => {
-          if (err) {
-            res.send(err)
-          }
-          else {
-            console.log(result)
-            res.send(result)
-          }
-        })
-      }
-    };
-
-
-
-    // router.get('/getOne/:id', async (req, res) => {
-    //   console.log(res)
-    //   try {
-    //     const data = await Model.findById(req.params.id);
-    //     res.json(data)
-    //   }
-    //   catch (error) {
-    //     res.status(401).json({ message: error.message })
-    //   }
-    // });
-
-
-
-    exports.addCourses = (req, res, next) => {
-      const id = req.params.id
-
-      const courseId = req.body.course_id
-      console.log("hello", id, courseId)
-      if (id && courseId) {
-        User.findByIdAndUpdate(id, { $push: { addCourses: courseId } }, (err, updateUser) => {
-          if (err)
-            res.send({ "error": err })
-          else
-            res.send({ status: "updated", user: updateUser })
-        })
-
+// student profile update
+exports.update = async (req, res, next) => {
+  const id = req.params.ids;
+  if (id) {
+    User.findByIdAndUpdate(id, req.body, (err, result) => {
+      if (err) {
+        res.send(err)
       }
       else {
-        res.send({ " error": "field are empty!" })
+        console.log(result)
+        res.send(result)
       }
-    };
+    })
+  }
+};
 
 
-  
+
+// router.get('/getOne/:id', async (req, res) => {
+//   console.log(res)
+//   try {
+//     const data = await Model.findById(req.params.id);
+//     res.json(data)
+//   }
+//   catch (error) {
+//     res.status(401).json({ message: error.message })
+//   }
+// });
+
+
+
+exports.addCourses = (req, res, next) => {
+  const id = req.params.id
+
+  const courseId = req.body.course_id
+  console.log("hello", id, courseId)
+  if (id && courseId) {
+    User.findByIdAndUpdate(id, { $push: { addCourses: courseId } }, (err, updateUser) => {
+      if (err)
+        res.send({ "error": err })
+      else
+        res.send({ status: "updated", user: updateUser })
+    })
+
+  }
+  else {
+    res.send({ " error": "field are empty!" })
+  }
+};
+
+
+
+
+
+
